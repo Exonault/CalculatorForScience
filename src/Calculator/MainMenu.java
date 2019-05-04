4e50080a246af6f83574bf287d0a1e680f1a9c69
PhoeniksKiller <35682388+PhoeniksKiller@users.noreply.github.com>
2019-05-04 13:52:40 +0300

    Update MainMenu.java

--- src/Calculator/MainMenu.java
+++ src/Calculator/MainMenu.java
@@ -1,7 +1,22 @@
 package Calculator;
-
-public class MainMenu {
-    public static void main(String[] args) {
-
+import java.util.Scanner;
+public class MainMenu 
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+		System.out.print("Vavedete 1 za programistki calculator\n");//za da se vidi che moje da se vika spokoino sam slojil tova
+        //pls no hate
+		int a=input.nextInt();
+		while(a!=1)
+		{
+			System.out.print("Vavedete nanovo\n");
+			a=input.nextInt();
+		}
+		if(a==1)
+		{
+			ProgrammingCalculator programmingCalculator=new ProgrammingCalculator();
+            //vikane na tozi calc
+		}
     }
 }
