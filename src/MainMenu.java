3cd0c73c5530c8caa8217441f0f23ce72583bcb0
PhoeniksKiller <35682388+PhoeniksKiller@users.noreply.github.com>
2019-05-21 17:34:46 +0300

    Update MainMenu.java

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -4,6 +4,31 @@ public class MainMenu
 {
     public static void main(String[] args)
     {
+        JFrame frame = new JFrame();
+		
+		
+		frame.setBounds(0, 0, 600,480);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		JMenu CalcMenu = new JMenu("Calculators");
+		menuBar.add(CalcMenu);	
+		
+		JMenuItem PrCalc = new JMenuItem("Programming Calculator");
+		CalcMenu.add(PrCalc);
+		JMenuItem ScCalc = new JMenuItem("Scientific Calculator");
+		CalcMenu.add(ScCalc);
+		JMenuItem StCalc = new JMenuItem("Standart Calculator");
+		CalcMenu.add(StCalc);
+        //slojete kodovete ot drygite LayoutiNaCalcolatorite otdolo:
+        
+        
+        
+		
 
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
     }
 }
