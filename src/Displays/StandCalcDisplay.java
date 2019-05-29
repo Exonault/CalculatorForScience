6a07a1cf4641c92fd21377c85cab4eb2e8a2e190
PhoeniksKiller <35682388+PhoeniksKiller@users.noreply.github.com>
2019-05-29 08:14:16 +0300

    Update StandCalcDisplay.java
    Би трябвало да е готово

--- src/Displays/StandCalcDisplay.java
+++ src/Displays/StandCalcDisplay.java
@@ -1,67 +1,217 @@
-package src.Displays;
-
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JPanel;
+import javax.swing.JFrame;
 import javax.swing.JTextField;
 
-public class StandCalcDisplay {
-	public StandCalcDisplay() {
-		/*JTextField display = new JTextField();
-		display.setEditable( false );
-		display.setHorizontalAlignment(JTextField.RIGHT);
+public class StandCalcDisplay implements ActionListener
+{
+	JFrame f;
+	JTextField t;
+	JButton b1,b2,b3,b4,b5,b6,b7,b8,b9,b0,bdiv,bmul,bsub,badd,bdec,beq,bdel,bclr;
+	
+	static double a=0,b=0,result=0;
+	static int operator=0;
+	
+	StandCalcDisplay()
+	{
+		f = new JFrame("Standart Calculator");
+		t = new JTextField("");
+		b1 = new JButton("1");
+		b2 = new JButton("2");
+		b3 = new JButton("3");
+		b4 = new JButton("4");
+		b5 = new JButton("5");
+		b6 = new JButton("6");
+		b7 = new JButton("7");
+		b8 = new JButton("8");
+		b9 = new JButton("9");
+		b0 = new JButton("0");
+		bdiv = new JButton("/");
+		bmul = new JButton("*");
+		bsub = new JButton("-");
+		badd = new JButton("+");
+		bdec = new JButton(".");
+		beq = new JButton("=");
+		bdel = new JButton("Delete");
+		bclr = new JButton("CE");
 		
-		JPanel buttonPanel = new JPanel();
-		buttonPanel.setLayout( new GridLayout(0, 4) );
+		t.setBounds(30,40,280,30);
+		b7.setBounds(30,100,50,40);
+		b8.setBounds(100,100,50,40);
+		b9.setBounds(170,100,50,40);
+		bdiv.setBounds(240,100,50,40);
 		
-		JButton[] buttons=new JButton[20];
-		buttonPanel.add(new JButton("CE"));
-		buttonPanel.add(new JButton("/"));
-		buttonPanel.add(new JButton("*"));
-		buttonPanel.add(new JButton("-"));
+		b4.setBounds(30,170,50,40);
+		b5.setBounds(100,170,50,40);
+		b6.setBounds(170,170,50,40);
+		bmul.setBounds(240,170,50,40);
+		
+		b1.setBounds(30,240,50,40);
+		b2.setBounds(100,240,50,40);
+		b3.setBounds(170,240,50,40);
+		bsub.setBounds(240,240,50,40);
+		
+		bdec.setBounds(30,310,50,40);
+		b0.setBounds(100,310,50,40);
+		beq.setBounds(170,310,50,40);
+		badd.setBounds(240,310,50,40);
+		
+		bdel.setBounds(55,380,100,40);
+		bclr.setBounds(175,380,100,40);
+		
+		f.add(t);
+		f.add(b7);
+		f.add(b8);
+		f.add(b9);
+		f.add(bdiv);
+		f.add(b4);
+		f.add(b5);
+		f.add(b6);
+		f.add(bmul);
+		f.add(b1);
+		f.add(b2);
+		f.add(b3);
+		f.add(bsub);
+		f.add(bdec);
+		f.add(b0);
+		f.add(beq);
+		f.add(badd);
+		f.add(bdel);
+		f.add(bclr);
+		
+		f.setLayout(null);
+		f.setVisible(true);
+		f.setSize(350, 500);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setResizable(false);
+		f.setLocationRelativeTo(null);
 
-		for (int i = 1; i < 4; i++)
-		{
-			String text = String.valueOf(i);
-			 JButton button = new JButton( text ); 		
-			 button.setFont(new Font("Arial", Font.PLAIN, 40));
-			 button.setMnemonic( text.charAt(0) );
-			 buttons[i] = button;
-			 buttonPanel.add( button );
-		}
-		buttonPanel.add(new JButton("+") );
-		for (int i = 4; i < 7; i++)
-		{
-			String text = String.valueOf(i);
-			 JButton button = new JButton( text ); 		
-			 button.setFont(new Font("Arial", Font.PLAIN, 40));
-			 button.setMnemonic( text.charAt(0) );
-			 buttons[i] = button;
-			 buttonPanel.add( button );
-		}
-		buttonPanel.add(new JButton("√"));
-		for (int i = 7; i < 10; i++)
-		{
-			String text = String.valueOf(i);
-			 JButton button = new JButton( text ); 	
-			 button.setFont(new Font("Arial", Font.PLAIN, 40));
-			 button.setMnemonic( text.charAt(0) );
-			 buttons[i] = button;
-			 buttonPanel.add( button );
-		}
-		buttonPanel.add(new JButton("x*x"));
-		buttonPanel.add(new JButton("±"));
-		buttonPanel.add(new JButton("0"));
-		buttonPanel.add(new JButton("."));
-		buttonPanel.add(new JButton("="));
-		buttonPanel.setSize(new Dimension(35,15));
-		frame.getContentPane().add(display, BorderLayout.NORTH);
-		frame.getContentPane().add(buttonPanel);
+		b1.addActionListener(this);
+		b2.addActionListener(this);
+		b3.addActionListener(this);
+		b4.addActionListener(this);
+		b5.addActionListener(this);
+		b6.addActionListener(this);
+		b7.addActionListener(this);
+		b8.addActionListener(this);
+		b9.addActionListener(this);
+		b0.addActionListener(this);
+		badd.addActionListener(this);
+		bdiv.addActionListener(this);
+		bmul.addActionListener(this);
+		bsub.addActionListener(this);
+		bdec.addActionListener(this);
+		beq.addActionListener(this);
+		bdel.addActionListener(this);
+		bclr.addActionListener(this);
 	}
-*/
+	public void actionPerformed(ActionEvent e)
+	{
+		if(e.getSource()==b1)
+			t.setText(t.getText().concat("1"));
+		
+		if(e.getSource()==b2)
+			t.setText(t.getText().concat("2"));
+		
+		if(e.getSource()==b3)
+			t.setText(t.getText().concat("3"));
+		
+		if(e.getSource()==b4)
+			t.setText(t.getText().concat("4"));
+		
+		if(e.getSource()==b5)
+			t.setText(t.getText().concat("5"));
+		
+		if(e.getSource()==b6)
+			t.setText(t.getText().concat("6"));
+		
+		if(e.getSource()==b7)
+			t.setText(t.getText().concat("7"));
+		
+		if(e.getSource()==b8)
+			t.setText(t.getText().concat("8"));
+		
+		if(e.getSource()==b9)
+			t.setText(t.getText().concat("9"));
+		
+		if(e.getSource()==b0)
+			t.setText(t.getText().concat("0"));
+		
+		if(e.getSource()==bdec)
+			t.setText(t.getText().concat("."));
+		
+		if(e.getSource()==badd)
+		{
+			a=Double.parseDouble(t.getText());
+			operator=1;
+			t.setText("");
+		} 
+		
+		if(e.getSource()==bsub)
+		{
+			a=Double.parseDouble(t.getText());
+			operator=2;
+			t.setText("");
+		}
+		
+		if(e.getSource()==bmul)
+		{
+			a=Double.parseDouble(t.getText());
+			operator=3;
+			t.setText("");
+		}
+		
+		if(e.getSource()==bdiv)
+		{
+			a=Double.parseDouble(t.getText());
+			operator=4;
+			t.setText("");
+		}
+		
+		if(e.getSource()==beq)
+		{
+			b=Double.parseDouble(t.getText());
+		
+			switch(operator)
+			{
+//				case 1: StandardCalculator.sum(a,b);
+			case 1: result=a+b;
+					break;
+		
+//				case 2: StandardCalculator.diff(a,b);
+				case 2: result=a-b;
+					break;
+		
+//				case 3: StandardCalculator.multi(a,b);
+				case 3: result=a*b;
+					break;
+		
+//				case 4: StandardCalculator.dev(a,b);
+				case 4: result=a/b;
+					break;
+		
+				default: result=0;
+			}
+
+			t.setText(""+result);
+		}
+		
+		if(e.getSource()==bclr)
+			t.setText("");
+		
+		if(e.getSource()==bdel)
+		{
+			String s=t.getText();
+			t.setText("");
+			for(int i=0;i<s.length()-1;i++)
+			t.setText(t.getText()+s.charAt(i));
+		}		
+	}
+	
+	public static void main(String[] args)
+	{
+		new StandCalcDisplay();
 	}
 }
